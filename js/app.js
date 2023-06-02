cca8e4b85485b1f3feebbf1cd9aff0fba941f543
Raul Rodriguez <raulrobi@gmail.com>
2023-06-01 23:39:32 -0300

    SW routes modified

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-var url = window.location.href;
-var swLocation = "/twittor-pwa/sw.js";
-
 if (navigator.serviceWorker) {
-    if (url.includes("localhost")) {
-        swLocation = "/sw.js";
-    }
-    navigator.serviceWorker.register(swLocation);
+    navigator.serviceWorker.register("/sw.js");
 }
 
 // Referencias de jQuery
