c357c10051d16478349e1191a993ecedb00712d8
Raul Rodriguez <raulrobi@gmail.com>
2023-06-02 18:59:51 -0300

    SW routes modified

--- js/app.js
+++ js/app.js
@@ -122,3 +122,24 @@ postBtn.on("click", function () {
 
     crearMensajeHTML(mensaje, usuario);
 });
+
+// This variable will save the event for later use.
+let defferedPrompt;
+const addbtn = document.querySelector(".btn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    defferedPrompt = event;
+    addbtn.style.display = "block";
+});
+
+addbtn.addEventListener("click", (event) => {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then((choice) => {
+        if (choice.outcome === "accepted") {
+            console.log("user accepted the prompt");
+        }
+        defferedPrompt = null;
+    });
+});
--- sw.js
+++ sw.js
@@ -90,24 +90,3 @@ self.addEventListener("fetch", (event) => {
 
     event.respondWith(resp);
 });
-
-// This variable will save the event for later use.
-let defferedPrompt;
-const addbtn = window.document.querySelector(".btn");
-
-window.addEventListener("beforeinstallprompt", (event) => {
-    event.preventDefault();
-    defferedPrompt = event;
-    addbtn.style.display = "block";
-});
-
-addbtn.addEventListener("click", (event) => {
-    defferedPrompt.prompt();
-
-    defferedPrompt.userChoice.then((choice) => {
-        if (choice.outcome === "accepted") {
-            console.log("user accepted the prompt");
-        }
-        defferedPrompt = null;
-    });
-});
