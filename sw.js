379880022fe5009fa75de300a5cdcc6b88262d37
Raul Rodriguez <raulrobi@gmail.com>
2023-06-02 17:37:07 -0300

    SW routes modified

--- sw.js
+++ sw.js
@@ -7,16 +7,16 @@ const INMUTABLE_CACHE = "inmutable-v1";
 //Aquí se declara todo lo que es de mi aplicación
 const APP_SHELL = [
     "/",
-    "index.html",
-    "css/style.css",
-    "img/favicon.ico",
-    "img/avatars/spiderman.jpg",
-    "img/avatars/hulk.jpg",
-    "img/avatars/ironman.jpg",
-    "img/avatars/thor.jpg",
-    "img/avatars/wolverine.jpg",
-    "js/app.js",
-    "js/sw-utils.js"
+    "/index.html",
+    "/css/style.css",
+    "/img/favicon.ico",
+    "/img/avatars/spiderman.jpg",
+    "/img/avatars/hulk.jpg",
+    "/img/avatars/ironman.jpg",
+    "/img/avatars/thor.jpg",
+    "/img/avatars/wolverine.jpg",
+    "/js/app.js",
+    "/js/sw-utils.js"
 ];
 
 //Aquí va todo lo que no se va a modificar jamás
